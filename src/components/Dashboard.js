cf9f2505a63cef1808b2c7b3badd4845ac1b0838
Codelleschi <a.steinhauslin@gmail.com>
2022-07-10 10:58:26 -0700

    created dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <header className="Dashboard-header">
+        <h1>What's in your wallet</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
